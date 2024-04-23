d6dc896e75e139a1c04a97582b9f65344a722900
Tobias Bocanegra <tripod@adobe.com>
2024-04-23 23:54:18 +0200

    add polygons

--- blob.gd
+++ blob.gd
@@ -5,27 +5,70 @@ class_name Blob
 @onready var points: Node2D = $points
 @onready var polys: Node2D = $polys
 
-class Line:
-  var p0:Vector2
-  var p1:Vector2
+#  Vertex: 1.2         Edge  .0.
+#          . .               3 1
+#          8.4               .2.
+#
+#  0 ...   4 ...   8 ...  12 ...
+#    . .     . /     \ .     ===
+#    ...     ./*     *\.     *.*
+#
+#  1 */.   5 */.   9 *|.  13 *\.
+#    / .     / /     .|.     . \
+#    ...     ./*     *|.     *.*
+#
+#  2 .\*   6 .|*  10 .\*  14 ./*
+#    . \     .|.     \ \     / .
+#    ...     .|*     *\.     *.*
+#
+#  3 *.*   7 *.*  11 *.*  15 *.*
+#    ===     \ .     . /     . .
+#    ...     .\*     */.     *.*
+#
 
 const edge_table = [
-  [],       # 0
-  [0, 3],   # 1
-  [1, 0],   # 2
-  [1, 3],   # 3
-  [2, 1],   # 4
-  [2, 1, 0, 3], # 5
-  [2, 0],   # 6
-  [2, 3],   # 7
-  [3, 2],   # 8
-  [0, 2],   # 9
-  [3, 2, 1, 0],   # 10
-  [1, 2],   # 11
-  [3, 1],   # 12
-  [0, 1],   # 13
-  [3, 0],   # 14
-  [],   # 15
+  [], # 0
+  [3, 0], # 1
+  [0, 1], # 2
+  [3, 1], # 3
+  [1, 2], # 4
+  [3, 0, 1, 2], # 5
+  [0, 2], # 6
+  [3, 2], # 7
+  [2, 3], # 8
+  [2, 0], # 9
+  [0, 1, 2, 3], # 10
+  [2, 1], # 11
+  [1, 3], # 12
+  [1, 0], # 13
+  [0, 3], # 14
+  [], # 15
+]
+
+const connection_table = [
+  [9, 9, 9, 9], # 0
+  [9, 9, 9, 0], # 1
+  [1, 9, 9, 9], # 2
+  [9, 9, 9, 1], # 3
+  [9, 2, 9, 9], # 4
+  [9, 2, 9, 0], # 5
+  [2, 9, 9, 9], # 6
+  [9, 9, 9, 2], # 7
+  [9, 9, 3, 9], # 8
+  [9, 9, 0, 9], # 9
+  [1, 9, 3, 9], # 10
+  [9, 9, 1, 9], # 11
+  [9, 3, 9, 9], # 12
+  [9, 0, 9, 9], # 13
+  [3, 9, 9, 9], # 14
+  [9, 9, 9, 9], # 15
+]
+
+const edge_direction:Array[Vector2i] = [
+  Vector2i(0, -1),
+  Vector2i(1, 0),
+  Vector2i(0, 1),
+  Vector2i(-1, 0),
 ]
 
 var selected:BlobPoint
@@ -43,6 +86,11 @@ var selected:BlobPoint
 @export var show_grid:bool = false:
   set(value):
     show_grid = value
+    recalc()
+
+@export var generate_polygons:bool = false:
+  set(value):
+    generate_polygons = value
     recalc()
 
 func _ready():
@@ -75,97 +123,86 @@ func sdf(x:float, y:float)->float:
   return dist
 
 func commit_poly(pts:Array[Vector2])->void:
-  if pts.size() > 0:
+  if pts.size() > 2:
     var poly:Polygon2D = Polygon2D.new()
     poly.set_polygon(PackedVector2Array(pts))
     polys.add_child(poly)
     if Engine.is_editor_hint():
       poly.owner = get_tree().edited_scene_root
 
+func calcSDF(pos:Quaternion)->Quaternion:
+  var s0 = sdf(pos.x, pos.y)
+  var s1 = sdf(pos.z, pos.y)
+  var s2 = sdf(pos.z, pos.w)
+  var s3 = sdf(pos.x, pos.w)
+  return Quaternion(s0, s1, s2, s3)
+
+func calcEdgeIndex(sdf:Quaternion)->int:
+  return (1 if sdf.x >= limit else 0) \
+       + (2 if sdf.y >= limit else 0) \
+       + (4 if sdf.z >= limit else 0) \
+       + (8 if sdf.w >= limit else 0);
+
+# the s(df) quaternion holds the sdf values for all 4 corners
+# the p(os) quaternion holds the x0, y0, x1, y1 coordinates of the 4 corners
+func calcEdges(s:Quaternion, p:Quaternion)->Array[Vector2]:
+  var e0 = Vector2(     remap(limit, s.x, s.y, p.x, p.z), p.y)
+  var e1 = Vector2(p.z, remap(limit, s.y, s.z, p.y, p.w))
+  var e2 = Vector2(     remap(limit, s.z, s.w, p.z, p.x), p.w)
+  var e3 = Vector2(p.x, remap(limit, s.w, s.x, p.w, p.y))
+  return [e0, e1, e2, e3]
+
 func calc_lines()->void:
   for p in polys.get_children():
     polys.remove_child(p)
     p.queue_free()
-  var pts:Array[Vector2] = []
   var visited:Dictionary = {}
-  var next:Vector2i
-  var has_next:bool = false
-
-  var min_x:float = INF
-  var min_y:float = INF
-  var max_x:float = -INF
-  var max_y:float = -INF
   for p:BlobPoint in points.get_children():
-    min_x = minf(min_x, p.position.x - p.radius)
-    min_y = minf(min_y, p.position.y - p.radius)
-    max_x = maxf(max_x, p.position.x + p.radius)
-    max_y = maxf(max_y, p.position.y + p.radius)
-  var r0 = -3
-  var r1 = ceil((max_y - min_y) / cell_size) + 3
-  var q0 = -3
-  var q1 = ceil((max_x - min_x) / cell_size) + 3
+    var pts:Array[Vector2] = []
+    # start in the center of the blob and find the edge
+    var cell = Vector2i(p.position.x / cell_size, p.position.y / cell_size)
+    var hue = 0.0
+    var from = -1
+    while true:
+      var rect = Quaternion(cell.x, cell.y, cell.x + 1, cell.y + 1) * cell_size
+      var s = calcSDF(rect)
+      var idx = calcEdgeIndex(s)
+      if idx == 15:
+        # check the next cell below
+        if show_grid && !visited.has(cell):
+          var dc = sdf(rect.x + cell_size / 2, rect.y + cell_size / 2)
+          draw_rect(Rect2(rect.x, rect.y, cell_size, cell_size), Color.YELLOW, dc >= limit)
+        cell.y += 1
+        assert(cell.y < 1000)
+        continue
 
-  var r = r0
-  var q = q0
-  var hue:float = 0
-  while q < q1:
-    if !has_next:
-      next = Vector2i(q, r)
-      r += 1
-      if r >= r1:
-        r = r0
-        q += 1
-    has_next = false
-    if visited.has(next):
-      continue
-    visited[next] = true
-    var y0 = min_y + (next.y + 1) * cell_size
-    var y1 = min_y + next.y * cell_size
-    var x0 = min_x + next.x * cell_size
-    var x1 = min_x + (next.x + 1) * cell_size
-    if show_grid:
-      var dc = sdf(x0 + cell_size / 2, y1 + cell_size / 2)
-      draw_rect(Rect2(x0, y1, cell_size, cell_size), Color.CHOCOLATE, dc >= limit)
-    var s0 = sdf(x0, y0)
-    var s1 = sdf(x1, y0)
-    var s2 = sdf(x1, y1)
-    var s3 = sdf(x0, y1)
-    var idx:int = 1 if s0 >= limit else 0
-    idx += 2 if s1 >= limit else 0
-    idx += 4 if s2 >= limit else 0
-    idx += 8 if s3 >= limit else 0
-    if idx == 0 or idx == 15:
-      continue
-    var e0 = Vector2(remap(limit, s0, s1, x0, x1), y0)
-    var e1 = Vector2(x1, remap(limit, s1, s2, y0, y1))
-    var e2 = Vector2(remap(limit, s2, s3, x1, x0), y1)
-    var e3 = Vector2(x0, remap(limit, s3, s0, y1, y0))
-    var edge = edge_table[idx]
-    var edges = [e0, e1, e2, e3]
-    var color = Color.from_hsv(hue, 1.0, 1.0)
-    hue += 0.01
-    const edge_direction:Array[Vector2i] = [
-      Vector2i(0, 1),
-      Vector2i(1, 0),
-      Vector2i(0, -1),
-      Vector2i(-1, 0),
-    ]
-    if edge.size() > 0:
-      draw_line(edges[edge[0]], edges[edge[1]], color, 0.5, true)
-      pts.push_back(edges[edge[0]])
-      pts.push_back(edges[edge[1]])
-      next += edge_direction[edge[1]]
-      if visited.has(next):
-        commit_poly(pts)
-        pts = []
+      var edges = calcEdges(s, rect)
+      var color = Color.from_hsv(hue, 1.0, 1.0)
+      hue += 0.01
+      var edge = edge_table[idx]
+      if from < 0:
+        from = edge[0]
+
+      if show_grid && !visited.has(cell):
+        var dc = sdf(rect.x + cell_size / 2, rect.y + cell_size / 2)
+        draw_rect(Rect2(rect.x, rect.y, cell_size, cell_size), Color.CHOCOLATE, dc >= limit)
+
+      if visited.has(cell):
+        if visited[cell] & 2**from:
+          commit_poly(pts)
+          break;
+        visited[cell] |= 2**from
       else:
-        has_next = true
-    if edge.size() > 2:
-      draw_line(edges[edge[2]], edges[edge[3]], color, 0.5, true)
-      #pts.push_back(edges[edge[2]])
-      #pts.push_back(edges[edge[3]])
-
-  commit_poly(pts)
+        visited[cell] = 2**from
+      var to = connection_table[idx][from]
+      draw_line(edges[from], edges[to], color, 0.5, true)
+      if generate_polygons:
+        if pts.is_empty():
+          pts.push_back(edges[from])
+        pts.push_back(edges[to])
+      # TODO: optimize and only recalc new corners
+      cell += edge_direction[to]
+      from = (to + 2) % 4
 
 func recalc()->void:
   queue_redraw()
@@ -179,3 +216,19 @@ func _on_btn_add_pressed() -> void:
   b.local_transform_changed.connect(recalc)
   b.clicked.connect(_on_point_clicked.bind(b))
   select(b, true)
+
+func _on_range_spin_value_changed(value: float) -> void:
+  cell_size = value
+
+func _on_btn_remove_pressed() -> void:
+  if selected:
+    points.remove_child(selected)
+    selected.queue_free()
+    selected = null
+    recalc()
+
+func _on_btn_grid_toggled(toggled_on: bool) -> void:
+  show_grid = toggled_on
+
+func _on_btn_polygons_toggled(toggled_on: bool) -> void:
+  generate_polygons = toggled_on
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+butler push exports/web tripod/marching-squares:html5
+butler push exports/linux tripod/marching-squares:linux
